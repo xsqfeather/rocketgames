87915442b6cfe2324b2d52436e0d3792af1167f9
SimonXU <xsqfeather@gmail.com>
2024-01-04 20:01:58 +0800

    endpoint

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,3 @@
-export const ENDPOINT =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:3000"
-    : "https://games-api.8kas.com";
+export const ENDPOINT = import.meta.env.VITE_ENDPOINT as string;
 export const fetcher = (...args: [string, ...any]) =>
   fetch(...args).then((res) => res.json());
